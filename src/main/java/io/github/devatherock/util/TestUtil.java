9d1be6a15a4f9f863acffaaf31d3172fd7250589
devatherock <devatherock@gmail.com>
2023-03-22 20:52:35 -0500

    SO-75812949: WebClient error handling using onErrorResume

--- src/main/java/io/github/devatherock/util/TestUtil.java
+++ src/main/java/io/github/devatherock/util/TestUtil.java
@@ -1,8 +1,45 @@
 package io.github.devatherock.util;
 
+import java.util.List;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import reactor.core.publisher.Mono;
+
 public class TestUtil {
 
     public static String sayHello() {
         return "Hello";
+    }
+    
+    public static List<String> makeHttpRequest() {
+    	WebClient webClient = WebClient.builder().baseUrl("http://localhost:8081")
+    			.build();
+    	String reference = "1234";
+    	
+    	ObjectMapper objectMapper = new ObjectMapper(); 
+    	List<String> response = webClient
+    	  .post()
+    	  .uri("/validation/{reference}", reference)
+    	  .retrieve()
+    	  .bodyToMono(new ParameterizedTypeReference<List<String>>() {})
+    	  .onErrorResume(WebClientResponseException.class, ex -> {
+				try {
+					return ex.getRawStatusCode() == 422 ? 
+							Mono.just(objectMapper.readValue(ex.getResponseBodyAsString(), new TypeReference<List<String>>() {})) : 
+								Mono.error(ex);
+				} catch (JsonProcessingException jsonException) {
+					return Mono.error(jsonException);
+				}
+			})
+    	  .block();
+    	
+    	return response;
     }
 }
--- src/test/groovy/io/github/devatherock/util/TestUtilSpec.groovy
+++ src/test/groovy/io/github/devatherock/util/TestUtilSpec.groovy
@@ -1,11 +1,45 @@
 package io.github.devatherock.util
 
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock
+
+import groovy.json.JsonOutput
+
+import spock.lang.Shared
 import spock.lang.Specification
 
 class TestUtilSpec extends Specification {
+
+    @Shared
+    WireMockServer mockServer = new WireMockServer(8081)
+
+    void setupSpec() {
+        WireMock.configureFor(8081)
+        mockServer.start()
+    }
+
+    void cleanupSpec() {
+        mockServer.stop()
+    }
+
+    void cleanup() {
+        mockServer.resetAll()
+    }
     
     void 'test say hello'() {
         expect:
         TestUtil.sayHello() == 'Hello'
+    }
+
+    void 'test make http request'() {
+        given:
+        def responseBody = ['line1', 'line2', 'line3']
+        WireMock.givenThat(WireMock.post('/validation/1234')
+                .willReturn(WireMock.aResponse()
+                        .withStatus(422)
+                        .withBody(JsonOutput.toJson(responseBody))))
+
+        expect:
+        TestUtil.makeHttpRequest() == responseBody
     }
 }
