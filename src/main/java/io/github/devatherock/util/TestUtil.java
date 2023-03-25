dbd6d5b257218c26d542e26f8775cf3b6bad83c3
devatherock <devatherock@gmail.com>
2023-03-25 17:48:33 -0500

    SO-75823934: JDBC resultset - assign managers to employees

--- src/main/java/io/github/devatherock/util/TestUtil.java
+++ src/main/java/io/github/devatherock/util/TestUtil.java
@@ -1,8 +1,77 @@
 package io.github.devatherock.util;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 public class TestUtil {
 
-    public static String sayHello() {
-        return "Hello";
-    }
+	public static String sayHello() {
+		return "Hello";
+	}
+
+	public static Set<Employee> transformResultSet(ResultSet resultSet) throws SQLException {
+		Set<Employee> employees = new LinkedHashSet<>();
+		Map<BigInteger, Employee> employeeMap = new HashMap<>();
+		Map<BigInteger, BigInteger> employeeToManagerMap = new HashMap<>();
+
+		while (resultSet.next()) {
+			BigInteger id = BigInteger.valueOf(resultSet.getInt(1));
+			FullName fullName = new FullName(resultSet.getString("firstName"), resultSet.getString("lastName"),
+					resultSet.getString("middleName"));
+			Position position = Position.valueOf(resultSet.getString("position"));
+			LocalDate hired = resultSet.getDate(7).toLocalDate();
+			BigDecimal salary = resultSet.getBigDecimal("salary");
+			BigInteger managerId = BigInteger.valueOf(resultSet.getInt(6));
+
+			if (managerId != null && !managerId.equals(BigInteger.ZERO)) {
+				employeeToManagerMap.put(id, managerId);
+			}
+			
+			Employee employee = new Employee(id, fullName, position, hired, salary, null);
+			employees.add(employee);
+			employeeMap.put(id, employee);
+		}
+		
+		employeeToManagerMap.forEach((key, value) -> {
+			Employee employee = employeeMap.get(key);
+			Employee manager = employeeMap.get(employeeToManagerMap.get(key));
+			employee.setManager(manager);
+		});
+		
+		return employees;
+	}
+	
+	@Getter
+	@Setter
+	@AllArgsConstructor
+	public static class Employee {
+		private BigInteger id;
+		private FullName fullName;
+		private Position position;
+		private LocalDate hireDate;
+		private BigDecimal salary;
+		private Employee manager;
+	}
+
+	@AllArgsConstructor
+	public static class FullName {
+		private String firstName;
+		private String lastName;
+		private String middleName;
+	}
+
+	public static enum Position {
+		EMPLOYEE, MANAGER
+	}
 }
--- src/test/groovy/io/github/devatherock/util/TestUtilSpec.groovy
+++ src/test/groovy/io/github/devatherock/util/TestUtilSpec.groovy
@@ -2,10 +2,45 @@ package io.github.devatherock.util
 
 import spock.lang.Specification
 
+import java.sql.Date
+import java.sql.ResultSet
+import io.github.devatherock.util.TestUtil.Employee
+
 class TestUtilSpec extends Specification {
     
     void 'test say hello'() {
         expect:
         TestUtil.sayHello() == 'Hello'
+    }
+
+    void 'test transform result set'() {
+        given:
+        ResultSet resultSet = Mock()
+        long currentTime = System.currentTimeMillis()
+
+        when:
+        Set<Employee> employeesSet = TestUtil.transformResultSet(resultSet)
+
+        then: 'set resultset size'
+        3 * resultSet.next() >>> [true, true, false]
+
+        and: 'data rows'
+        2 * resultSet.getInt(1) >>> [1, 2]
+        2 * resultSet.getString('firstName') >>> ['first1', 'first2']
+        2 * resultSet.getString('lastName') >>> ['last1', 'last2']
+        2 * resultSet.getString('middleName') >>> ['middle1', 'middle2']
+        2 * resultSet.getString('position') >>> ['EMPLOYEE', 'MANAGER']
+        2 * resultSet.getDate(7) >>> [new Date(currentTime), new Date(currentTime)]
+        2 * resultSet.getBigDecimal('salary') >>> [new BigDecimal('10000'), new BigDecimal('15000')]
+        2 * resultSet.getInt(6) >>> [2, 0]
+
+        and: 'verify result'
+        employeesSet.size() == 2
+        def employees = new ArrayList<Employee>(employeesSet)
+        employees[0].id == 1
+        employees[0].manager
+        employees[0].manager.id == 2
+        employees[1].id == 2
+        !employees[1].manager
     }
 }
