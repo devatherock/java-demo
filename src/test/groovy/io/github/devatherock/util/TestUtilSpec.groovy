504e561821e8800242f12ed8d6db11eda28e3251
devatherock <devatherock@gmail.com>
2023-05-09 09:49:05 -0500

    SO-76178871: Simple JAXB + spring-oxm xml unmarshaller

--- src/test/groovy/io/github/devatherock/util/TestUtilSpec.groovy
+++ src/test/groovy/io/github/devatherock/util/TestUtilSpec.groovy
@@ -1,11 +1,37 @@
 package io.github.devatherock.util
 
+import org.springframework.oxm.jaxb.Jaxb2Marshaller
 import spock.lang.Specification
 
+import javax.xml.transform.stream.StreamSource
+
 class TestUtilSpec extends Specification {
-    
+
     void 'test say hello'() {
         expect:
         TestUtil.sayHello() == 'Hello'
+    }
+
+    void 'test jaxb unmarshaller'() {
+        given:
+        Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller()
+        jaxb2Marshaller.setClassesToBeBound(Generic)
+
+        and:
+        String soapResponse =
+        '''
+        <ns2:GenericRequest xmlns:ns2="http://www.oracle.com/UCM">
+            <ns2:Service>testService</ns2:Service>
+        </ns2:GenericRequest>
+        '''.stripIndent()
+
+        when:
+        def object = jaxb2Marshaller.createUnmarshaller()
+                .unmarshal(new StreamSource(new StringReader(soapResponse)), Generic)
+        def response = object.value
+
+        then:
+        response instanceof Generic
+        response.service == 'testService'
     }
 }
