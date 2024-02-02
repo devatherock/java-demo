6a52eb89276001557092e20a70237d185fe6e1d5
devatherock <devatherock@gmail.com>
2024-02-01 21:11:40 -0600

    SO-77433874: Converted a BsonArray into a JSON

--- src/main/java/io/github/devatherock/util/TestUtil.java
+++ src/main/java/io/github/devatherock/util/TestUtil.java
@@ -1,8 +1,61 @@
 package io.github.devatherock.util;
 
+import lombok.val;
+import org.bson.BsonArray;
+import org.bson.BsonBoolean;
+import org.bson.BsonDocument;
+import org.bson.BsonInt32;
+import org.bson.BsonString;
+import org.bson.BsonValue;
+import org.bson.json.JsonWriterSettings;
+
 public class TestUtil {
 
-    public static String sayHello() {
-        return "Hello";
-    }
+	public static String sayHello() {
+		return "Hello";
+	}
+
+	public static String toJson(BsonArray bsonArray) {
+		String json = null;
+
+		if (null != bsonArray) {
+			StringBuilder jsonBuilder = new StringBuilder("[");
+
+			for (int index = 0; index < bsonArray.size(); index++) {
+				BsonValue value = bsonArray.get(index);
+
+				if (value instanceof BsonInt32) {
+					jsonBuilder.append(((BsonInt32) value).getValue());
+				} else if (value instanceof BsonDocument) {
+					jsonBuilder.append(((BsonDocument) value).toJson());
+				} else if (value instanceof BsonArray) {
+					jsonBuilder.append(toJson((BsonArray) value));
+				} else if (value instanceof BsonBoolean) {
+					jsonBuilder.append(((BsonBoolean) value).getValue());
+				} else if (value instanceof BsonString) {
+					jsonBuilder.append('"');
+					jsonBuilder.append(((BsonString) value).getValue());
+					jsonBuilder.append('"');
+				}
+				// TODO Handle other required BsonValue data types like BsonInt64, BsonDouble,
+				// BsonTimestamp, etc
+
+				if (index != bsonArray.size() - 1) {
+					jsonBuilder.append(", ");
+				}
+			}
+
+			jsonBuilder.append(']');
+			json = jsonBuilder.toString();
+		}
+
+		return json;
+	}
+
+	public static String toJsonHack(BsonArray bsonArray) {
+		BsonDocument temp = new BsonDocument("x", bsonArray);
+		String json = temp.toJson(JsonWriterSettings.builder().indent(true).build());
+
+		return json.substring(9, json.length() - 1);
+	}
 }
--- src/test/groovy/io/github/devatherock/util/TestUtilSpec.groovy
+++ src/test/groovy/io/github/devatherock/util/TestUtilSpec.groovy
@@ -1,5 +1,10 @@
 package io.github.devatherock.util
 
+import org.bson.BsonArray
+import org.bson.BsonBoolean
+import org.bson.BsonDocument
+import org.bson.BsonInt32
+import org.bson.BsonString
 import spock.lang.Specification
 
 class TestUtilSpec extends Specification {
@@ -7,5 +12,42 @@ class TestUtilSpec extends Specification {
     void 'test say hello'() {
         expect:
         TestUtil.sayHello() == 'Hello'
+    }
+
+    void 'test to json'() {
+        expect:
+        TestUtil.toJson(testArray()) == '[1, 2, {"key_one": 3}, [4], false, "hello"]'
+    }
+
+    void 'test to json hack'() {
+        given:
+        String expected = '''
+        [
+            1,
+            2,
+            {
+              "key_one": 3
+            },
+            [
+              4
+            ],
+            false,
+            "hello"        
+          ]'''.stripIndent().trim()
+
+        expect:
+        TestUtil.toJsonHack(testArray()).trim() == expected
+    }
+
+    private BsonArray testArray() {
+        BsonArray bsonArray = new BsonArray()
+        bsonArray.add(new BsonInt32(1))
+        bsonArray.add(new BsonInt32(2))
+        bsonArray.add(new BsonDocument("key_one", new BsonInt32(3)))
+        bsonArray.add(new BsonArray([new BsonInt32(4)]))
+        bsonArray.add(new BsonBoolean(false))
+        bsonArray.add(new BsonString("hello"))
+
+        return bsonArray
     }
 }
