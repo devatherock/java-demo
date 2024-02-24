b698ebc8f53210fb81353696903fa36d42e53ea9
devatherock <devatherock@gmail.com>
2024-02-23 18:54:05 -0600

    SO-71887058: Retried parameterized junit tests using rerunner-jupiter

--- src/test/java/io/github/devatherock/util/TestUtilTest.java
+++ src/test/java/io/github/devatherock/util/TestUtilTest.java
@@ -1,12 +1,24 @@
 package io.github.devatherock.util;
 
+import io.github.artsok.ParameterizedRepeatedIfExceptionsTest;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class TestUtilTest {
+	private static int errorCounter = 0;
+	private static int resultIndex = 0;
+	private static List<Integer> args = Arrays.asList(14, 15, 100, -10);
 
-	@Test
-	public void testSayHello() {
-		Assertions.assertEquals("Hello", TestUtil.sayHello());
+	@ParameterizedRepeatedIfExceptionsTest(repeats = 3)
+	@ValueSource(ints = {14, 15, 100, -10})
+	public void testRepeat(int argument) {
+		if ((argument == 14 || argument == -10) && errorCounter++ < 2) {
+			throw new RuntimeException("Testing repeat");
+		}
+		errorCounter = 0;
+		Assertions.assertEquals(args.get(resultIndex++), argument);
 	}
 }
